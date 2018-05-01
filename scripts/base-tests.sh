ff33d1746cd10d8dc9006e07edf5365992b3580a
Connor Abdelnoor <connor@ergon.tech>
2018-05-01 11:18:16 -0700

    refactor basic ansible test script

--- scripts/base-tests.sh
+++ scripts/base-tests.sh
@@ -1,41 +1,68 @@
 #!/bin/bash
 set -e
 
-## Set up vars for Docker setup.
-distro="ubuntu1604"
-init="/lib/systemd/systemd"
-opts="--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
+# A script to run a set of basic tests with ansible
+# ------
+# Tests
+#
+# 1. Syntax check
+# 2. Playbook runs
+# 3. Playbook is idempotent
+
+# Args
+container_id=
+playbook="test.yml"
+path_to_playbook="/etc/ansible/roles/test-role/tests"
+
+usage()
+{
+    printf "usage:
+     --container-id   -id    docker container id
+     --playbook       -b     playbook to run (default: test.yml)
+     --path           -p     path-to-playbook (default: /etc/ansible/roles/test-role/tests) \n"
+}
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -id | --container-id )  shift
+                                container_id=$1
+                                ;;
+        -b | --playbook )       shift
+                                playbook=$1
+                                ;;
+        -p | --path )           shift
+                                path_to_playbook=$1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+    shift
+done
+
+
+
+## Constants
 red='\033[0;31m'
 green='\033[0;32m'
 neutral='\033[0m'
-#TODO remove hardcoding
-container_id='test-role-container'
-
-# Inputs
-playbook="test.yml"
-role_dir=$PWD
-
-# Run the container using the supplied OS.
-printf ${green}"Starting Docker container: geerlingguy/docker-$distro-ansible."${neutral}"\n"
-docker pull geerlingguy/docker-$distro-ansible:latest
-docker run --detach --volume="$role_dir":/etc/ansible/roles/role_under_test:rw --name $container_id $opts geerlingguy/docker-$distro-ansible:latest $init
-
-printf "\n"
 
 # Test Ansible syntax.
 printf ${green}"Checking Ansible playbook syntax."${neutral}
-docker exec --tty $container_id ansible-playbook /etc/ansible/roles/role_under_test/tests/$playbook --syntax-check
+docker exec --tty $container_id ansible-playbook ${path_to_playbook}/${playbook}--syntax-check
 
 printf "\n"
 
 # Run Ansible playbook.
-printf ${green}"Running command: docker exec $container_id ansible-playbook /etc/ansible/roles/role_under_test/tests/$playbook"${neutral}
-docker exec $container_id env ANSIBLE_FORCE_COLOR=1 ansible-playbook /etc/ansible/roles/role_under_test/tests/$playbook
+printf ${green}"Running command: docker exec $container_id ansible-playbook ${path_to_playbook}/${playbook}"${neutral}
+docker exec $container_id env ANSIBLE_FORCE_COLOR=1 ansible-playbook ${path_to_playbook}/${playbook}
 
 # Run Ansible playbook again (idempotence test).
 printf ${green}"Running playbook again: idempotence test"${neutral}
 idempotence=$(mktemp)
-docker exec $container_id ansible-playbook /etc/ansible/roles/role_under_test/tests/$playbook | tee -a $idempotence
+docker exec $container_id ansible-playbook ${path_to_playbook}/${playbook}| tee -a $idempotence
 tail $idempotence \
     | grep -q 'changed=0.*failed=0' \
     && (printf ${green}'Idempotence test: pass'${neutral}"\n") \
