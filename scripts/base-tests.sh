ff3af8678513b444de3639eeb8c48cd2dadad65b
Connor Abdelnoor <connor@ergon.tech>
2018-05-01 09:45:07 -0700

    update script for now

--- scripts/base-tests.sh
+++ scripts/base-tests.sh
@@ -8,7 +8,8 @@ opts="--privileged --volume=/sys/fs/cgroup:/sys/fs/cgroup:ro"
 red='\033[0;31m'
 green='\033[0;32m'
 neutral='\033[0m'
-container_id=${container_id:-$(date +%s)}
+#TODO remove hardcoding
+container_id='test-role-container'
 
 # Inputs
 playbook="test.yml"
@@ -23,13 +24,13 @@ printf "\n"
 
 # Test Ansible syntax.
 printf ${green}"Checking Ansible playbook syntax."${neutral}
-docker exec --tty $container_id env TERM=xterm ansible-playbook /etc/ansible/roles/role_under_test/tests/$playbook --syntax-check
+docker exec --tty $container_id ansible-playbook /etc/ansible/roles/role_under_test/tests/$playbook --syntax-check
 
 printf "\n"
 
 # Run Ansible playbook.
-printf ${green}"Running command: docker exec $container_id env TERM=xterm ansible-playbook /etc/ansible/roles/role_under_test/tests/$playbook"${neutral}
-docker exec $container_id env TERM=xterm env ANSIBLE_FORCE_COLOR=1 ansible-playbook /etc/ansible/roles/role_under_test/tests/$playbook
+printf ${green}"Running command: docker exec $container_id ansible-playbook /etc/ansible/roles/role_under_test/tests/$playbook"${neutral}
+docker exec $container_id env ANSIBLE_FORCE_COLOR=1 ansible-playbook /etc/ansible/roles/role_under_test/tests/$playbook
 
 # Run Ansible playbook again (idempotence test).
 printf ${green}"Running playbook again: idempotence test"${neutral}
